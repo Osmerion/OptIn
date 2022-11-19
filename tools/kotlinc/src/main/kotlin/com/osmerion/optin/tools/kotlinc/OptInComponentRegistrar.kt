1ec2a1da431a6d5f116ee3ff4577c2b7c30cdbd2
Leon Linhart <themrmilchmann@gmail.com>
2022-11-19 13:30:51 +0100

    feat(tools.kotlinc): disable K2 support

--- tools/kotlinc/src/main/kotlin/com/osmerion/optin/tools/kotlinc/OptInComponentRegistrar.kt
+++ tools/kotlinc/src/main/kotlin/com/osmerion/optin/tools/kotlinc/OptInComponentRegistrar.kt
@@ -5,17 +5,23 @@
 package com.osmerion.optin.tools.kotlinc
 
 import com.osmerion.optin.tools.kotlinc.fir.OptInPluginRegistrar
-import com.osmerion.optin.tools.kotlinc.resolve.diagnostics.OsmerionOptInDiagnosticSuppressor
 import org.jetbrains.kotlin.com.intellij.mock.MockProject
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
-import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
 
 class OptInComponentRegistrar : ComponentRegistrar {
 
+    /*
+     * The plugin works by suppressing diagnostics reported by the default
+     * opt-in checks and replacing them with our custom ones. However, this is
+     * currently only possible in K1 since there is no API for suppressing
+     * diagnostics in FIR yet.
+     *
+     * https://youtrack.jetbrains.com/issue/KT-55028
+     */
     override val supportsK2: Boolean
-        get() = true
+        get() = false
 
     override fun registerProjectComponents(project: MockProject, configuration: CompilerConfiguration) {
         FirExtensionRegistrar.registerExtension(project, OptInPluginRegistrar())
